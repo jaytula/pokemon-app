90f54c7dcca3ee12cce6fd241a3ba2c5c4708750
Jay T <jaytula@gmail.com>
2021-03-24 09:19:42 -0700

    Added img to Pokemon component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {
+  findByAltText,
+  getByAltText,
+  getByRole,
+  render,
+  screen,
+} from "@testing-library/react";
+import App from "./App";
+import { extractPokemonURL } from "./components/Pokemon";
+import Pokemon from "./components/Pokemon";
 
+describe("Pokemon.tsx", () => {
+  test("extracts the url from the params", () => {
+    expect(extractPokemonURL("https://pokeapi.co/api/v2/pokemon/1/")).toBe(1);
+    expect(extractPokemonURL("https://pokeapi.co/api/v2/pokemon/2/")).toBe(2);
+    expect(extractPokemonURL("https://pokeapi.co/api/v2/pokemon/bad/")).toBe(
+      NaN
+    );
+  });
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
+  test("renders an image of the right pokemon", () => {
+    const wrapper = render(
+      <Pokemon name="bulbasaur" url={"https://pokeapi.co/api/v2/pokemon/1/"} />
+    );
+    const imageElem = wrapper.getByAltText("bulbasaur alt text");
+  });
+});
--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,25 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import classes from './Pokemon.module.css'
+import React, { useEffect, useState } from "react";
+import classes from "./Pokemon.module.css";
 
 interface Props {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
+}
+
+export const extractPokemonURL: (url: string) => number = (url) => {
+  const segments = url.split('/')
+  return Number(segments[segments.length - 2])
 }
 
 // https://pokeres.bastionbot.org/
 
-const Pokemon: React.FC<Props> = ({name, url}) => {
-    const [info, setInfo] = useState<any>()
-    useEffect(() => {
-      fetch(url)
-        .then(res => res.json())
-        .then(data => {
-          setInfo(data);
-        })
-    }, [url])
-    return <div className={classes.root}>
+const Pokemon: React.FC<Props> = ({ name, url}) => {
+  const id = extractPokemonURL(url);
+  const [info, setInfo] = useState<any>();
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setInfo(data);
+      });
+  }, [url]);
+  return (
+    <div className={classes.root}>
       {name} : <a href={url}>More Info</a>
+      <img src={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`} alt={`${name} alt text`} />
     </div>
-}
+  );
+};
 
 export default Pokemon;
